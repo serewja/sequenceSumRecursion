71722510ffd775d6e1f920e1b67baab13fb25a92
Sergei Shinkarenko <serewja@gmail.com>
2020-07-11 16:36:08 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,12 @@
-import f from "../dist/index.js"
+import sequenceSum from "../dist/index.js"
 
-test('testname', () => {
-    
+test('border cases', () => {
+  expect(sequenceSum(7, 2)).toBe(NaN);
+  expect(sequenceSum(0, 0)).toBe(0);
+  expect(sequenceSum(5, 5)).toBe(5);
 });
+test('standart cases', () => {
+  expect(sequenceSum(1, 5)).toBe(15);
+  expect(sequenceSum(4, 10)).toBe(49);
+  expect(sequenceSum(-3, 2)).toBe(-3);
+})
